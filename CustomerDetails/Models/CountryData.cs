dac07b6f59d816132ad1e0fc5ee8ee2dc9929637
Chinmaya.0patra <chinmay.0patra@gmail.com>
2024-04-23 04:04:58 +0530

    Add project files.

---
+++ CustomerDetails/Models/CountryData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace CustomerDetails.Models
+{
+    public class CountryData
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+     
+        public string Pincode { get; set; }
+        
+        public string Country { get; set; }
+        
+    }
+}
---
+++ CustomerDetails/Models/PagedListViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerDetails.Models
+{
+    public class PagedListViewModel<T>
+    {
+        public IEnumerable<T> PagedItems { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPages
+        {
+            get { return (int)System.Math.Ceiling((double)TotalCount / PageSize); }
+        }
+
+        public PagedListViewModel(IEnumerable<T> pagedItems, int pageNumber, int pageSize, int totalCount)
+        {
+            PagedItems = pagedItems;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalCount = totalCount;
+        }
+    }
+}
